f73abd6d47ee7267e6e7cd5540a581a09fd5db55
Piotr Vladimir <piotrvladimir89@gmail.com>
2017-02-24 18:49:10 +0100

    added key event

--- ng-app2/src/app/app.component.ts
+++ ng-app2/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  heroes = [
+	  new Hero(1, 'Windstorm'),
+	  new Hero(13, 'Bombasto'),
+	  new Hero(15, 'Magneta'),
+	  new Hero(20, 'Tornado')
+	];
+	myHero = this.heroes[0];
+
+// click event
+  clickMessage = '';
+
+  onClickMe() {
+    this.clickMessage = 'You are my hero!';
+  }
+
+// key event
+  values = '';
+
+  /* onKey(event: any) { // without type info
+    this.values += event.target.value + ' | ';
+  } */
+
+  // key event 1 (event param)
+  onKey(event: KeyboardEvent) { // with type info
+    this.values += (<HTMLInputElement>event.target).value + ' | ';
+  }
 }
