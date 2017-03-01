255d0c99deb903d75130be8387806d6941e544e6
Piotr Vladimir <piotrvladimir89@gmail.com>
2017-03-01 08:34:58 +0100

    updated angular samples

---
+++ angular-router2/app/highlight.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({ selector: '[highlight]' })
+/** Highlight the attached element in gold */
+export class HighlightDirective {
+  constructor(el: ElementRef) {
+    el.nativeElement.style.backgroundColor = 'gold';
+    console.log(
+      `* AppRoot highlight called for ${el.nativeElement.tagName}`);
+  }
+}
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
